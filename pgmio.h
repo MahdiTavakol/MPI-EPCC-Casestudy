ff323a78aef235400f94132bcaecceff97a0640b
MahdiTavakol <61837861+MahdiTavakol@users.noreply.github.com>
2024-08-10 13:59:06 +0100

    Update pgmio.h

--- pgmio.h
+++ pgmio.h
@@ -1,7 +1,12 @@
-template (typename type)
+template <typename type>
 void deallocate(type ** &v);
-template (typename type)
+template <typename type>
 type **allocate(type ** &v, const int nx, const int ny);
+template <typename type>
+type **allocate_cpp(type ** &v, const int nx, const int ny)
+template <typename type>
+void deallocate_cpp(type ** &v);
+
 void pgmsize (char *filename, int *nx, int *ny);
 void pgmread (char *filename, void *vx, int nx, int ny);
 void pgmwrite(char *filename, void *vx, int nx, int ny);
