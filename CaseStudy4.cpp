fe85aaa54770edfa6baa430b9c4179c014dee336
MahdiTavakol <61837861+MahdiTavakol@users.noreply.github.com>
2024-08-10 14:34:32 +0100

    Update CaseStudy4.cpp

--- CaseStudy4.cpp
+++ CaseStudy4.cpp
@@ -1,5 +1,4 @@
 #include <string>
-#include <cstring>
 #include <iostream>
 #include "mpi.h"
 #include "pgmio.h"
@@ -12,10 +11,9 @@ int main(int argc, char** argv)
     int N;
     int Mp;
     int Np;
-	  double *masterbuff, *buff, *new_ptr, *old, *edge;
-    string filename;
-    char* filename_c;
-    char* outputfilename_c;
+    double *masterbuff, *buff, *new_ptr, *old, *edge;
+    std::string filename;
+    std::string outputfilename;
 
     M = stoi(argv[1]);
     N = stoi(argv[2]);
@@ -35,14 +33,14 @@ int main(int argc, char** argv)
     filename = "edge" + to_string(M) + "x" + to_string(N) + ".pgm";
     pgmsize(filename, &M, &N);
 
-    Mp = M/size;
-    Np = N;
+    Mp = M;
+    Np = N/size;
 
-    masterbuff = new double[M*N];
-    buff       = new double[Mp*Np];
-    new_ptr    = new double[(Mp+2)*(Np+2)];
-    old        = new double[(Mp+2)*(Np+2)];
-    edge       = new double[(Mp+2)*(Np+2)];
+    masterbuff = allocate(masterbuff,M,N);
+    buff       = allocate(masterbuff,Mp,Np);
+    new_ptr    = allocate(new_ptr,Mp+2,Np+2);
+    old        = allocate(old,Mp+2,Np+2);
+    edge       = allocate(edge,Mp+2,Np+2);
 
 
     MPI_Barrier(world);
@@ -59,19 +57,14 @@ int main(int argc, char** argv)
         }
         if (rank == 0)
         {
-            pgmread(filename_c,masterbuff,M,N);
+            pgmread(filename,masterbuff,M,N);
             cout << "Finished reading " << filename  << endl;
         }
-        MPI_Scatter(masterbuff,Mp*Np,MPI_DOUBLE,buff,Mp*Np,MPI_DOUBLE,0,world);
+        MPI_Scatter(masterbuff[0],Mp*Np,MPI_DOUBLE,buff[0],Mp*Np,MPI_DOUBLE,0,world);
         for (int i = 0; i < Mp; i++)
-        {
             for (int j = 0; j < Np; j++)
-                {
-                    int indx0 = (Np+2)-(j+1)-1+(Np+2)*(i+1);
-                    int indx1 = Np - j  -1 + Np*i;
-                    edge[indx0] = buff[indx1];
-                }
-        }
+		edge[i+1][j+1] = buff[i][j];
+	    
         for (int l = 0; l < iteration; l++)
         {
             //cout << k << " step " << l << " out of " << iteration << endl;
@@ -80,21 +73,11 @@ int main(int argc, char** argv)
                 {
                     //cout << k << " step " << l << " out of " << iteration << endl;
                     //cout << "Working on " << i << "," << j << endl;
-                    int indx0 = (Np+2)-  j  -1+(Np+2)*i;
-                    int indx1 = (Np+2)-  j  -1+(Np+2)*(i-1);
-                    int indx2 = (Np+2)-  j  -1+(Np+2)*(i+1);
-                    int indx3 = (Np+2)-(j-1)-1+(Np+2)*i;
-                    int indx4 = (Np+2)-(j+1)-1+(Np+2)*i;
-                    int indx5 = (Np+2)-  j  -1+(Np+2)*i;
-                    //cout << "Indexes are " << indx0 << "," << indx1 << "," << indx2 << "," << indx3 << "," << indx4 << "," << indx5 << endl;
-                    new_ptr[indx0] = 0.25*(old[indx1]+old[indx2]+old[indx3]+old[indx4]-edge[indx5]);
+		    new_ptr[i][j] = 0.25*(old[i+1][j]+old[i-1][j]+old[i][j+1]+old[i][j-1]-edge[i][j]);
                 }
             for (int i = 1; i < Mp+1; i++)
                 for (int j = 1; j < Np+1; j++)
-                {
-                    int indx = (Np+2)-j-1+(Np+2)*i;
-                    old[indx] = new_ptr[indx];
-                }
+                    old[i][j] = new_ptr[i][j];
         }
 
         for (int i = 1; i < Mp+1; i++)
@@ -102,16 +85,14 @@ int main(int argc, char** argv)
             {
                 int indx0 = Np-(j-1)-1+Np*(i-1);
                 int indx1 = (Np+2) - j -1 + (Np+2)*i;
-                buff[indx0] = old[indx1];  
+                buff[i-1][j-1] = old[i][j];  
             }
 
-        MPI_Gather(buff, Mp*Np, MPI_DOUBLE,masterbuff,Mp*Np,MPI_DOUBLE,0,world);
+        MPI_Gather(buff[0], Mp*Np, MPI_DOUBLE,masterbuff[0],Mp*Np,MPI_DOUBLE,0,world);
         if (rank == 0)
         {
             string outputfilename = "edge" + to_string(M) + "x" + to_string(N) + "-iteration-" + to_string(iteration) + ".pgm";
-            outputfilename_c = new char[outputfilename.length()+1];
-            strcpy(outputfilename_c, outputfilename.c_str());
-            pgmwrite(outputfilename_c,masterbuff,M,N);
+            pgmwrite(outputfilename,masterbuff,M,N);
         }
     }
 
@@ -123,12 +104,13 @@ int main(int argc, char** argv)
         numIters = numIters + (float)iterations[i];
     float timing = (end - start)*1000.0 / numIters;
     if (rank == 0)
-		    printf("It takes %f ms on %d CPUs\n",timing,size);
-    
-    delete [] buff;
-    delete [] new_ptr;
-    delete [] old;
-    delete [] edge;
+	printf("It takes %f ms on %d CPUs\n",timing,size);
+
+    deallocate(masterbuff);
+    deallocate(buff);
+    deallocate(new_ptr);
+    deallocate(old);
+    deallocate(edge);
     MPI_Finalize();
 
     return 0;
