0143e8c77eb5e43b9a532ae75e08e9875b1af237
MahdiTavakol <61837861+MahdiTavakol@users.noreply.github.com>
2024-08-10 13:47:57 +0100

    Update pgmio.h

--- pgmio.h
+++ pgmio.h
@@ -1,3 +1,6 @@
+template (typename type)
+void deallocate(type ** &v)
+
 void pgmsize (char *filename, int *nx, int *ny);
 void pgmread (char *filename, void *vx, int nx, int ny);
 void pgmwrite(char *filename, void *vx, int nx, int ny);
